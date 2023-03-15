00a86c8d930290740c10fcdcf115b6b86f9d7f4e
Axizeaus <axizeaus@gmail.com>
2023-03-15 20:18:03 +0630

    listing view created

--- real_estate/listings/views.py
+++ real_estate/listings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Listing
 
-# Create your views here.
+def listing_list(request):
+    listings = Listing.objects.all()
+    context = {
+        'listings' : listings
+    }
+    return render(request, 'listings.html', context)
